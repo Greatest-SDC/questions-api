e1068be63e5703f04e77b6d41c1901c6bb954c25
Dani Tian <td.tiandani@gmail.com>
2021-03-24 11:44:48 -0700

    database host address merge conflict

--- database/index.js
+++ database/index.js
@@ -1,11 +1,15 @@
 const { Client } = require('pg');
 
-const client = new Client({
-  user: 'dantian',
-  host: '34.213.179.73',
-  database: 'sdc',
-  port: 5432,
-});
+const client = new Client(
+  // {
+  // user: 'postgres',
+  // password: 'postgres',
+  // host: '52.13.38.210',
+  // database: 'postgres',
+  // port: 5432,
+  'postgres://postgres:postgres@54.184.248.161:5432/postgres'
+// }
+);
 
 client.connect((err) => {
   if (err) {
